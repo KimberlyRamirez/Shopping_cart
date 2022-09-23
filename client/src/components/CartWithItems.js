05646e12c260aae022723a22abddd89ce84b006a
Kim Ramirez <kim.ram27@gmail.com>
2022-09-22 19:10:32 -0600

    fix tbody su=yntax error

--- client/src/components/CartWithItems.js
+++ client/src/components/CartWithItems.js
@@ -9,23 +9,27 @@ const CartWithItems = ({ products, checkoutHandler }) => {
     <div className="cart">
       <h2>Your Cart</h2>
       <table className="cart-items">
-        <tr>
-          <th>Item</th>
-          <th>Quantity</th>
-          <th>Price</th>
-        </tr>
-        {products.map((product, idx) => {
-          return (
-            <tr key={product+idx}>
-              <td>{product.title}</td>
-              <td>{product.quantity}</td>
-              <td>{product.price}</td>
-            </tr>
-          )
-        })}
-        <tr>
-          <td colSpan="3" className="total">Total: ${total.toFixed(2)}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+          {products.map((product, idx) => {
+            return (
+              <tr key={product+idx}>
+                <td>{product.title}</td>
+                <td>{product.quantity}</td>
+                <td>{product.price}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+        <tbody>
+          <tr>
+            <td colSpan="3" className="total">Total: ${total.toFixed(2)}</td>
+          </tr>
+        </tbody>
       </table>
       <a className="button checkout" onClick={checkoutHandler}>Checkout</a>
     </div>
